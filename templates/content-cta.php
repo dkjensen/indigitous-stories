c706fe99421ce51e30571824a945a55d52aca8d7
David Jensen <david@dkjensen.com>
2019-11-25 07:22:41 -0800

    Hided cta button if user is logged in

--- templates/content-cta.php
+++ templates/content-cta.php
@@ -17,7 +17,9 @@ if ( isset( $post->stories_grid_cta ) && $post->stories_grid_cta ) :
 <div <?php post_class( 'indigitous-story-cta' ); ?>>
     <div class="story-cta-content">
         <h3><?php _e( 'Indigitous is a global community advancing God\'s Kingdom through technology.', 'indigitous-stories' ); ?></h3>
+        <?php if ( ! is_user_logged_in() ) : ?>
         <p><a href="#register" data-modal="true" class="button"><?php _e( 'Meet Awesome People', 'indigitous-stories' ); ?></a></p>
+        <?php endif; ?>
     </div>
 </div>
 
