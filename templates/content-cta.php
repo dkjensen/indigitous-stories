9bb1c3184c558d622ad8f87cdf49895c1fb9bf0a
David Jensen <david@dkjensen.com>
2019-11-25 07:21:37 -0800

    Fix cta button

--- templates/content-cta.php
+++ templates/content-cta.php
@@ -17,7 +17,7 @@ if ( isset( $post->stories_grid_cta ) && $post->stories_grid_cta ) :
 <div <?php post_class( 'indigitous-story-cta' ); ?>>
     <div class="story-cta-content">
         <h3><?php _e( 'Indigitous is a global community advancing God\'s Kingdom through technology.', 'indigitous-stories' ); ?></h3>
-        <p><a href="#register" class="button"><?php _e( 'Meet Awesome People', 'indigitous-stories' ); ?></a></p>
+        <p><a href="#register" data-modal="true" class="button"><?php _e( 'Meet Awesome People', 'indigitous-stories' ); ?></a></p>
     </div>
 </div>
 
